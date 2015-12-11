a4d0d6cfa84bd945cff345156d498c45cd3a09cf
almir-garic <almir.garic.1992@ieee.org>
2015-12-11 23:07:05 +0100

    drugi upload

--- Lioness/Lioness/MainPage.xaml.cs
+++ Lioness/Lioness/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace Lioness
         {
             this.InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
