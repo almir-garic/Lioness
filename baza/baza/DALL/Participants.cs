90f22cde1ad4832d78bb94dd5b81bc27111b714a
almir-garic <almir.garic.1992@ieee.org>
2015-12-12 02:44:45 +0100

    cetvrtiUpload

---
+++ baza/baza/DALL/Participants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baza.DALL
+{
+   public class Participants
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int teamId { get; set; }
+        public virtual Team Team { get; set; }
+
+    }
+}
---
+++ baza/baza/DALL/bazadbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace baza.DALL
+{
+    public class bazadbContext: DbContext
+    {
+        public DbSet<Participants> Participant { get; set; }
+        public DbSet<Team> Teams { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbPath = ApplicationData.Current.LocalFolder.Path + "\\bazaPodatakaTest.db";
+            optionsBuilder.UseSqlite("Filename=" + dbPath);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
