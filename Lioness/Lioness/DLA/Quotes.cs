ddca362dfd363553a229fd9b0bb4e430d99eacd3
almir-garic <almir.garic.1992@ieee.org>
2015-12-12 08:25:40 +0100

    Dodano DLA.quotes

---
+++ Lioness/Lioness/DLA/Quotes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lioness.DLA
+{
+   public class Quotes
+    {
+        public int Id { get; set; }
+        public string Quote { get; set; }
+    }
+}
